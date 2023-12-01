e5a00171bec160eac57b1b59d0af4905acab3a03
SaturnCitizen <tristan.schorr@hotmail.fr>
2023-12-01 22:16:13 +0100

    movement + jump

---
+++ Assets/Scripts/JumpTestScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpTestScript : MonoBehaviour
+{
+    [Range(3, 10)]
+    public float jumpVelocity;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public float lateralSpeed = 5f;
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && rb.velocity.y == 0)
+        {
+            rb.velocity = Vector2.up * jumpVelocity;
+        }
+
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2((lateralSpeed * -1), rb.velocity.y);
+        } else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(lateralSpeed, rb.velocity.y);
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey("space"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+
+    }
+}
